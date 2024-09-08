4c555e4a43fdc324065c6e3b8d5e83d1c8c9bbbc
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2024-09-07 20:45:53 -0700

    Create command.sh

---
+++ GCMC_MD_Files/MD/RunningSimulations/command.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+for i in $(cat tmp.txt)
+do
+cd $i
+
+rm -rf Simulations
+
+if [[ ! -d Simulations ]];
+then
+mkdir Simulations
+cd Simulations
+
+cp ../combine.* .
+cp ../in_nvt .
+cp ../simulation.input .
+
+cp ../edit.* .
+
+#cp ../../run.sh .
+#sed -i 's/DIRR/'$i'/' run.sh
+
+cp ../../lammps.sh .
+cp ../../condor-sh .
+
+sed -i 's/XXX/'$i'/' lammps.sh
+sed -i 's/XXX/'$i'/' condor-sh
+
+condor_submit condor-sh
+
+cd ../../
+
+fi
+
+done
