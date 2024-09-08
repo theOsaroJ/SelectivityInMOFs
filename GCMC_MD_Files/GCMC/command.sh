4a5a9fcbd77a5d24fbcba8e1c83bccb512999789
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2024-09-07 20:16:07 -0700

    Create command.sh
    This is the job automated script to run many GCMC situations at several pressures, temperatures and mol fractions using RASPA

---
+++ GCMC_MD_Files/GCMC/command.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+for p in $(cat tmp.csv)
+do
+mkdir $p
+cd $p
+cp ../Essentials/temp.csv .
+
+for t in $(cat temp.csv)
+do
+mkdir $t
+cd $t
+cp ../../Essentials/mols.csv .
+
+for m in $(cat mols.csv)
+do
+mkdir $m
+cd $m
+
+cp ../../../Essentials/simulation.input .
+cp ../../../Essentials/condor-sh .
+cp ../../../Essentials/raspa.sh .
+
+sed -i 's/PPP/'$p'/' simulation.input
+sed -i 's/TTT/'$t'/' simulation.input
+sed -i 's/MMM/'$m'/' simulation.input
+
+sed -i 's/PPP/'$p'/' raspa.sh
+sed -i 's/TTT/'$t'/' raspa.sh
+sed -i 's/MMM/'$m'/' raspa.sh
+
+sed -i 's/PPP/'$p'/' condor-sh
+sed -i 's/TTT/'$t'/' condor-sh
+sed -i 's/MMM/'$m'/' condor-sh
+
+total=1
+rep=$(echo "$total - $m" | bc)
+
+sed -i 's/CCC/'$rep'/' simulation.input
+
+condor_submit condor-sh
+
+cd ../
+done
+
+cd ../
+done
+
+cd ../
+done
